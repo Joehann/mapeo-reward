a491cf35cda81b66dc1df7a8b1d0ea131c306b03
Joehann <jpouponnot@outlook.fr>
2025-03-02 19:47:18 +0100

    Add form layout to CreatePartnerPage
    Implemented a detailed input form with fields for partner details, utilizing React-Bootstrap components. Added a submit button which navigates back to the homepage after creation. This enhances the CreatePartnerPage with a functional UI for partner input.

--- src/react/pages/CreatePartner/index.tsx
+++ src/react/pages/CreatePartner/index.tsx
@@ -1,3 +1,67 @@
+import Form from "react-bootstrap/Form";
+import {Button, Col, Row} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
+
 export const CreatePartnerPage = () => {
-    return <div>Create Partner</div>
+    const navigate = useNavigate()
+
+    return <Form className={'col-md-4 mx-auto mt-5'}>
+        <Row className="mb-3">
+            <Col>
+                <Form.Label>Prénom</Form.Label>
+                <Form.Control/>
+            </Col>
+            <Col>
+                <Form.Label>Nom</Form.Label>
+                <Form.Control/>
+            </Col>
+        </Row>
+        <Row className="mb-3">
+            <Col>
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email"/>
+            </Col>
+            <Col>
+                <Form.Label>Téléphone</Form.Label>
+                <Form.Control/>
+            </Col>
+        </Row>
+        <Row className="mb-3">
+            <Col xs={6}>
+                <Form.Label>Adresse</Form.Label>
+                <Form.Control/>
+            </Col>
+            <Col xs={2}>
+                <Form.Label>Code postal</Form.Label>
+                <Form.Control/>
+            </Col>
+            <Col>
+                <Form.Label>Ville</Form.Label>
+                <Form.Control/>
+            </Col>
+        </Row>
+        <Row className="mb-3">
+            <Col xs={6}>
+                <Form.Label>Type d'apporteur</Form.Label>
+                <Form.Select aria-label="partner type select">
+                    <option disabled>Choisir un type</option>
+                    <option value="cgp">Conseiller en Gestion de Patrimoine</option>
+                    <option value="broker">Courtier</option>
+                    <option value="private-finder">Particulier</option>
+                    <option value="other">Autre</option>
+                </Form.Select>
+            </Col>
+            <Col>
+                <Form.Label>Précision</Form.Label>
+                <Form.Control/>
+            </Col>
+        </Row>
+
+        <Button className={'button-mapeo-green-outline mx-auto d-block mt-5'}
+                type="submit"
+                onClick={() => navigate('/')}
+        >
+            Créer un apporteur
+        </Button>
+    </Form>
 }
